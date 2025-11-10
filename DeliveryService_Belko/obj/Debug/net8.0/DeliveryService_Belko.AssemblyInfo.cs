b2b10dcc03e7a70d08e4714cfc8092d02dc27362
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-11-10 12:39:45 +0200

    фыфыфы

--- DeliveryService_Belko/obj/Debug/net8.0/DeliveryService_Belko.AssemblyInfo.cs
+++ DeliveryService_Belko/obj/Debug/net8.0/DeliveryService_Belko.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bb7a2bb6bebfcae81e63b9d0b41d31ea8ef6ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3580fa48e95cb3a544d5676d2b98d3cc4c29aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
