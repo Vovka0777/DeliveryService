3f5a4040fbb0e5546727968bceada5ac39a1beee
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-11-27 22:13:47 +0200

    ы

--- DeliveryService_Belko/obj/Debug/net8.0/DeliveryService_Belko.AssemblyInfo.cs
+++ DeliveryService_Belko/obj/Debug/net8.0/DeliveryService_Belko.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4c054128e8ddd38ad88c37138748e901437718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15735af3a7b7eb0f8f35ae957eb5906e19b0264")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryService_Belko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
